29fa14e7d29be1a01a0fd3f865ec359fd7b2c589
MaaviaAsghar4 <maavia.asghar.96@gmail.com>
2022-03-30 10:01:53 +0500

    removed warnings and errors

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,25 @@ extern crate diesel;
 
 extern crate dotenv;
 
-use diesel::pg::PgConnection;
-use dotenv::dotenv;
-use r2d2;
-use r2d2_diesel::ConnectionManager;
-
 use actix_web::{App, HttpServer};
 
 use std::{env, io};
 
+mod db;
 mod models;
 mod routes;
 mod schema;
 
-pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
-pub type DBPooledConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
+// pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+// pub type DBPooledConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
 
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set properly");
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
-
-    let pool = r2d2::Pool::new(manager).expect("Failed to create pool");
-
     HttpServer::new(move || {
         App::new()
-            .app_data(pool.get())
+            // .app_data(connection)
             .service(routes::get_all_books)
             .service(routes::get_book)
             .service(routes::insert_book)
--- src/routes.rs
+++ src/routes.rs
@@ -1,29 +1,66 @@
+use super::db;
 use super::models::{Book, NewBook};
-use crate::DBPooledConnection;
-use actix_web::{delete, get, post, put, web, HttpResponse};
-use serde_json::Value;
+use actix_web::{delete, get, post, put, web, HttpRequest, HttpResponse};
 
 #[get("/books")]
-pub async fn get_all_books(conn: &DBPooledConnection) -> HttpResponse {
-    HttpResponse::Ok().body("Hello world!")
+pub async fn get_all_books() -> HttpResponse {
+    let connection = db::establish_connection();
+    let books = Book::get_all_books(&connection);
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(books)
 }
 
 #[get("/book/{id}")]
-pub async fn get_book(conn: &DBPooledConnection) -> HttpResponse {
-    HttpResponse::Ok().body("Hello world!")
+pub async fn get_book(req: HttpRequest) -> HttpResponse {
+    let connection = db::establish_connection();
+    let id: i32 = req.match_info().query("id").parse().unwrap();
+    let books = Book::get_book(id, &connection);
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(books)
 }
 
 #[post("/books")]
-pub async fn insert_book(conn: &DBPooledConnection) -> HttpResponse {
-    HttpResponse::Ok().body("Hello world!")
+pub async fn insert_book(new_book: web::Json<NewBook>) -> HttpResponse {
+    let connection = db::establish_connection();
+    let new_book = new_book.into_inner();
+    let add_book = NewBook {
+        title: new_book.title,
+        author: new_book.author,
+    };
+    let books = Book::insert_book(add_book, &connection);
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(books)
 }
 
 #[put("/book/{id}")]
-pub async fn update_book(conn: &DBPooledConnection) -> HttpResponse {
-    HttpResponse::Ok().body("Hello world!")
+pub async fn update_book(req: HttpRequest, new_book: web::Json<NewBook>) -> HttpResponse {
+    let connection = db::establish_connection();
+    let new_book = new_book.into_inner();
+    let id: i32 = req.match_info().query("id").parse().unwrap();
+    let add_book = NewBook {
+        title: new_book.title,
+        author: new_book.author,
+    };
+    let books = Book::update_book(id, add_book, &connection);
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(books)
 }
 
 #[delete("/book/{id}")]
-pub async fn delete_book(conn: &DBPooledConnection) -> HttpResponse {
-    HttpResponse::Ok().body("Hello world!")
+pub async fn delete_book(req: HttpRequest) -> HttpResponse {
+    let connection = db::establish_connection();
+    let id: i32 = req.match_info().query("id").parse().unwrap();
+    let books = Book::delete_book(id, &connection);
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(books)
 }
